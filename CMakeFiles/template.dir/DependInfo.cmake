ec3db36f081e521db54b515608290433287d44db
Elias Almqvist <elalmqvist@gmail.com>
2023-09-14 14:13:23 +0200

    gitignore

---
+++ CMakeFiles/template.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/elal/Projects/school/cth/courses/datproj/template/startup.s" "/home/elal/Projects/school/cth/courses/datproj/template/CMakeFiles/template.dir/startup.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "include"
+  "include/perip"
+  "include/cmsis"
+  "include/src"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/elal/Projects/school/cth/courses/datproj/template/drivers/perip/src/misc.c" "CMakeFiles/template.dir/drivers/perip/src/misc.c.o" "gcc" "CMakeFiles/template.dir/drivers/perip/src/misc.c.o.d"
+  "/home/elal/Projects/school/cth/courses/datproj/template/drivers/perip/src/stm32f4xx_can.c" "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_can.c.o" "gcc" "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_can.c.o.d"
+  "/home/elal/Projects/school/cth/courses/datproj/template/drivers/perip/src/stm32f4xx_gpio.c" "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_gpio.c.o" "gcc" "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_gpio.c.o.d"
+  "/home/elal/Projects/school/cth/courses/datproj/template/drivers/perip/src/stm32f4xx_rcc.c" "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_rcc.c.o" "gcc" "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_rcc.c.o.d"
+  "/home/elal/Projects/school/cth/courses/datproj/template/lib/src/startup.c" "CMakeFiles/template.dir/lib/src/startup.c.o" "gcc" "CMakeFiles/template.dir/lib/src/startup.c.o.d"
+  "/home/elal/Projects/school/cth/courses/datproj/template/src/main.c" "CMakeFiles/template.dir/src/main.c.o" "gcc" "CMakeFiles/template.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/template.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template.dir/drivers/perip/src/misc.c.o"
+  "CMakeFiles/template.dir/drivers/perip/src/misc.c.o.d"
+  "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_can.c.o"
+  "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_can.c.o.d"
+  "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_gpio.c.o"
+  "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_gpio.c.o.d"
+  "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_rcc.c.o"
+  "CMakeFiles/template.dir/drivers/perip/src/stm32f4xx_rcc.c.o.d"
+  "CMakeFiles/template.dir/lib/src/startup.c.o"
+  "CMakeFiles/template.dir/lib/src/startup.c.o.d"
+  "CMakeFiles/template.dir/src/main.c.o"
+  "CMakeFiles/template.dir/src/main.c.o.d"
+  "CMakeFiles/template.dir/startup.s.o"
+  "template"
+  "template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
