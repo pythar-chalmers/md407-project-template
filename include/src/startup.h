622539ae334e6e653ad34b6e360018625743bd95
Elias Almqvist <elalmqvist@gmail.com>
2023-09-14 13:48:16 +0200

    fix: Fixed compilation bloat etc (now works on hardware)

--- include/src/startup.h
+++ include/src/startup.h
@@ -1,4 +1,17 @@
 #pragma once
 
-__attribute__((naked)) __attribute__((section(".start_section"))) void
-startup(void);
+// Constants
+#define BASE_STACK_ADDRESS 0x2001BFF0
+
+// Defines
+#ifndef str
+#define str(x) #x
+#endif
+
+#ifndef xstr
+#define xstr(x) str(x)
+#endif
+
+#define SET_STACK_ADDR(ADDR)                                                 \
+	__asm__ volatile(" LDR R0,=" xstr(ADDR) "\n");                           \
+	__asm__ volatile(" MOV SP,R0\n")
--- src/main.c
+++ src/main.c
@@ -1,8 +1,29 @@
-// #include <stdbool.h>
+/*
+ * Example LED with md407 project template
+ * */
 
-#define true 1
+// STD
+#include <stdbool.h>
+
+// Drivers/lib
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
 
 int main(void) {
+	GPIO_InitTypeDef init;
+	GPIO_StructInit(&init);
+
+	init.GPIO_Mode = GPIO_Mode_OUT;
+	init.GPIO_Pin  = GPIO_Pin_0;
+
+	// Init GPIO
+	GPIO_Init(GPIOD, &init);
+
+	// Enable the bits
+	GPIO_ResetBits(GPIOD, GPIO_Pin_0);
+	GPIO_SetBits(GPIOD, GPIO_Pin_0);
+
 	while (true)
 		;
 
